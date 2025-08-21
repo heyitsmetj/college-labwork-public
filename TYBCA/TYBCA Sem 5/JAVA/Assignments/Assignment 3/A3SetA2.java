1a5ec2a4781ca27d37aba14dd149a8ee85cb6573
heyitsmetj <tejasjadhav634@gmail.com>
2025-08-21 12:53:54 +0530

    updated JAVA

--- TYBCA/TYBCA Sem 5/JAVA/Assignments/Assignment 3/A3SetA2.java
+++ TYBCA/TYBCA Sem 5/JAVA/Assignments/Assignment 3/A3SetA2.java
@@ -1,59 +1,58 @@
 import java.util.*;
 import java.io.*;
 
-class A3SetA2
+class A3SetA2 
 {
-	public static void main(String args[])
-	{
-		HashMap <String, Float> emps = new HashMap<>();
-		
-		int n;
-		String name;
-		float salary;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("\nEnter the number of employees: ");
-		n = sc.nextInt();
-		
-		for(int i = 0; i<n; i++)
-		{
-			System.out.println("\nEnter Employee[" + i + "]:");
-			sc.nextLine();
-			
-			System.out.print("Enter Employee[" + i + "] Name: ");
-			name = sc.nextLine();
-			
-			System.out.print("Enter Employee[" + i + "] Salary: ");
-			salary = sc.nextFloat();
-			
-			emps.put(name, salary); 
-		}
-		
-		int i = 0;
-		
-		System.out.println("\nEmployee Details:");
-		System.out.println("\nName\tSalary\n");
-		
-		for(Map.Entry<String, Float> entry : emps.entrySet())
-		{
-			System.out.println(entry.getKey() + "\t" + entry.getValue());
-		}
-		
-		System.out.println("\nEmployee Details:\n" + emps);
-		
-		sc.nextLine();
-		System.out.print("\nYour Search Key: ");
-		String key = sc.nextLine();
-		
-		if(emps.containsKey(key))
-		{
-			System.out.println("Employee Name: " + key);
-			System.out.println("Employee Salary: " + emps.get(key));
-		}
-		else
-		{
-			System.out.println(key + " is NOT present in the Hash Table.");
-		}
-	}
+    public static void main(String args[]) 
+    {
+        Hashtable<String, Float> emps = new Hashtable<>();
+
+        int n;
+        String name;
+        float salary;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nEnter the number of employees: ");
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) 
+        {
+            System.out.println("\nEnter Employee[" + i + "]:");
+            sc.nextLine();
+
+            System.out.print("Enter Employee[" + i + "] Name: ");
+            name = sc.nextLine();
+
+            System.out.print("Enter Employee[" + i + "] Salary: ");
+            salary = sc.nextFloat();
+
+            emps.put(name, salary); 
+        }
+
+        System.out.println("\nEmployee Details:");
+        System.out.println("\nName\tSalary\n");
+
+        for (Map.Entry<String, Float> entry : emps.entrySet()) 
+        {
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+
+        System.out.println("\nEmployee Details:\n" + emps);
+
+        sc.nextLine();
+        System.out.print("\nYour Search Key: ");
+        String key = sc.nextLine();
+
+        if (emps.containsKey(key)) 
+        {
+            System.out.println("Employee Name: " + key);
+            System.out.println("Employee Salary: " + emps.get(key));
+        } 
+        else 
+        {
+            System.out.println(key + " is NOT present in the Hash Table.");
+        }
+    }
 }
+
