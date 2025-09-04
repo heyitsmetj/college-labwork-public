b1b9e949610b40fa33b49c0d29d18286fe1395f2
heyitsmetj <tejasjadhav634@gmail.com>
2025-09-04 13:44:35 +0530

    updated JAVA

--- TYBCA/TYBCA Sem 5/JAVA/Assignments/Assignment 4/A4SetA1.java
+++ TYBCA/TYBCA Sem 5/JAVA/Assignments/Assignment 4/A4SetA1.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class test
+public class A4SetA1
 {
 	public static void main(String args[])
 	{
